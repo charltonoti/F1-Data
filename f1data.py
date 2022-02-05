13841d3110c2baf9ff1110e4e4f4522a6f413dfb
Charlton <57676420+charltonoti@users.noreply.github.com>
2022-02-04 23:38:20 -0500

    Add files via upload

---
+++ f1data.py
@@ -0,0 +1,26 @@
+import csv 
+import pandas as pd 
+
+#circuit = pd.read_csv("circuit.csv")
+
+#circuit
+
+with open('circuits.csv') as circuits: 
+    #print(circuits.read())
+    #circuit_reader = csv.DictReader(circuits, delimiter = ' ')
+    #for row in circuits: 
+     #   print(row['name'])
+    
+    
+
+#circuits = open('circuits.csv')
+
+#csvreader = csv.reader(circuits)
+
+#header = []
+#header = next(csvreader)
+#header
+##rows = []
+#for row in csvreader:
+#        rows.append(row)
+#rows
